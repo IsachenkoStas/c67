086034ae9f8aa8068afcfec8679c8da8ce76da09
allencooper <isachello77@gmail.com>
2023-05-03 21:42:35 +0300

    main branch

--- src/GitClass.java
+++ src/GitClass.java
@@ -2,5 +2,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello 777");
         System.out.println("My second branch");
+        System.out.println("world");
     }
 }
