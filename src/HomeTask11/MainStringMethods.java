2281e6e74bf29fe3f83b0508254b46830516b225
allencooper <isachello77@gmail.com>
2023-05-30 15:17:08 +0300

    Palindrome Task final

--- src/HomeTask11/MainStringMethods.java
+++ src/HomeTask11/MainStringMethods.java
@@ -6,23 +6,18 @@ import java.util.Scanner;
 public class MainStringMethods {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        String[] arr = {"hello", "madam", "do", "you", "like", "radar"};
-        String line = "hello madam do you like radar";
+        String line = "hello madam do you like Radar";
         String[] arr = line.split(" ");
         int n = scanner.nextInt();
-        for (int i = 0; true; ) {
-            if (n <= arr.length) {
-                i = n - 1;
-                StringBuilder sBL = new StringBuilder(arr[i]);
-                sBL.reverse();
-                String res = sBL.toString();
-                if (res.equalsIgnoreCase(arr[i])) {
-                    System.out.println("This is palindrome!!");
-                } else {
-                    System.out.println("No, it's not palindrome");
-                }
+        if (n <= arr.length) {
+            StringBuilder sBL = new StringBuilder(arr[n - 1]);
+            sBL.reverse();
+            String res = sBL.toString();
+            if (res.equalsIgnoreCase(arr[n - 1])) {
+                System.out.println("This is palindrome!!");
+            } else {
+                System.out.println("No, it's not palindrome");
             }
-            break;
         }
     }
 }
