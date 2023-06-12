65f757f181dcfce93f7d57dd90bcae36c7005ed5
allencooper <isachello77@gmail.com>
2023-06-12 19:20:03 +0300

    Home Task 14. updated

--- src/HomeTask14/MainHT14.java
+++ src/HomeTask14/MainHT14.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 
 public class MainHT14 {
     public static void main(String[] args) throws IOException {
-        FileReader fileReader = new FileReader("/Users/allencooper/Desktop/romeo-and-juliet.txt");
+/*        FileReader fileReader = new FileReader("/Users/allencooper/Desktop/romeo-and-juliet.txt");
         int i;
         StringBuilder txt = new StringBuilder();
         while ((i = fileReader.read()) != -1) {
             txt.append((char) i);
         }
         fileReader.close();
-        String[] arr = txt.toString().replaceAll("\\W", " ").split(" ");
+        String[] arr = txt.toString().split("\\W");
         String max = arr[0];
         for (String s : arr) {
             if (s.length() > max.length()) {
@@ -24,27 +24,28 @@ public class MainHT14 {
 
         FileWriter fw = new FileWriter("/Users/allencooper/Desktop/max.txt");
         fw.write(max);
-        fw.close();
+        fw.close();*/
 
 
-//        FileReader fr = new FileReader("/Users/allencooper/Desktop/docnums.txt"));
-//        int i;
-//        StringBuilder docNumsTxt = new StringBuilder();
-//        while ((i = fr.read()) != -1) {
-//            docNumsTxt.append((char) i);
-//        }
-//        fr.close();
-//        String[] array = docNumsTxt.toString().split("\\s");
-//        for (String s : array) {
-//            if (s.matches("docnum\\w{9}") || s.matches("contract\\w{7}")) {
-//                FileWriter fileWriter = new FileWriter("/Users/allencooper/Desktop/validDocNums.txt", true);
-//                fileWriter.write(s + "\n");
-//                fileWriter.close();
-//            } else {
-//                FileWriter fileWriter1 = new FileWriter("/Users/allencooper/Desktop/notValidDocNums.txt", true);
-//                fileWriter1.write(s + " - не подходит по условиям валидности\n");
-//                fileWriter1.close();
-//            }
-//        }
+        FileReader fr = new FileReader("/Users/allencooper/Desktop/docnums.txt");
+        int i;
+        StringBuilder docNumsTxt = new StringBuilder();
+        while ((i = fr.read()) != -1) {
+            docNumsTxt.append((char) i);
+        }
+        fr.close();
+        FileWriter fileWriter = new FileWriter("/Users/allencooper/Desktop/validDocNums.txt", true);
+        FileWriter fileWriter1 = new FileWriter("/Users/allencooper/Desktop/notValidDocNums.txt", true);
+        String[] array = docNumsTxt.toString().split("\\s");
+        for (String s : array) {
+            if (s.matches("docnum\\w{9}") || s.matches("contract\\w{7}")) {
+                fileWriter.write(s + "\n");
+
+            } else {
+                fileWriter1.write(s + " - не подходит по условиям валидности\n");
+            }
+            fileWriter.close();
+            fileWriter1.close();
+        }
     }
 }
