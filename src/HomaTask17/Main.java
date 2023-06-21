89d66635d2c0ba3efc5873b3015e8e6f6775e955
allencooper <isachello77@gmail.com>
2023-06-21 12:26:41 +0300

    HT17

---
+++ src/HomaTask17/Main.java
@@ -0,0 +1,41 @@
+package HomaTask17;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+/*        Task 1
+        System.out.print("Enter your date of birth in the format 'yyyy-mm-dd' : ");
+        System.out.println("Вам будет сто лет - " + LocalDate.parse(new Scanner(System.in).nextLine()).plusYears(100));
+
+        Task 2
+        int[] numbers = {1, 5, -10, 3, -45, 500, -1000, 4355, -900, 900, -21312, 23, -45};
+        for (int number : numbers) {
+            Predicate<Integer> posNumbs = (num) -> num > 0;
+            if (posNumbs.test(number)) {
+                System.out.print(number + " ");
+            }
+        }
+
+        Task 3
+
+        Function<String, Double> fl = (a) -> Double.parseDouble(a) / 2.94;
+        System.out.println(fl.apply(new Scanner(System.in).nextLine()) + " $");
+
+        Task 4
+        Consumer<String> cl = (a) -> System.out.println(Double.parseDouble(a) / 2.94);
+        cl.accept(new Scanner(System.in).nextLine());
+
+        Task 5
+        Supplier<String> sl = () -> new StringBuilder(new Scanner(System.in).nextLine()).reverse().toString();
+        System.out.println(sl.get());*/
+    }
+}
